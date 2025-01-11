fc83c75ae74208df4dbcdcb67351c09906a545a5
Vladyslav Bilinskiy <drdokrod32@gmail.com>
2025-01-11 15:00:13 +0200

    Add custom section and font

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-
-const paths = {
-  scss: './assets/scss/**/*.scss',
-  css: './assets',
-};
 
 function styles() {
-  return gulp.src(paths.scss)
+  return gulp.src('assets/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer()) 
-    .pipe(cleanCSS()) 
-    .pipe(gulp.dest(paths.css));
+    .pipe(gulp.dest('assets'));
 }
 
 function watch() {
-  gulp.watch(paths.scss, styles);
+  gulp.watch('assets/**/*.scss', styles);
 }
 
-exports.default = gulp.series(styles, watch);
+exports.styles = styles;
+exports.watch = watch;
